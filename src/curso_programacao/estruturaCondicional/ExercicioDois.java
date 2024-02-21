85f364815f1e919d41abf3c2dca9b440e3620ad6
eoyago <eoyago>
2024-02-20 22:48:26 -0300

    melhorando exercicio 2

--- src/curso_programacao/estruturaCondicional/ExercicioDois.java
+++ src/curso_programacao/estruturaCondicional/ExercicioDois.java
@@ -6,20 +6,18 @@ public class ExercicioDois {
 
 	public static void main(String[] args) {
 		// Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar.
-		Scanner sc = new Scanner(System.in);		
-		
-		int number;
-		
-		number = sc.nextInt();
-		
-		if (number == -27) {
-			System.out.println("IMPAR");	
-		}
-		else if (number == 12 || number == 0 ){
-			System.out.println("PAR");
-		}
-				
-		sc.close();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = sc.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é ímpar.");
+        }
+
+        sc.close();
 	}
 
 }
